9ea997e2e125f2b15e0a011a4c1931b383c83949
naderaj <naderaj@mail.uc.edu>
2019-09-23 20:33:54 -0400

    fixing the sidebar toggle

--- js/global.js
+++ js/global.js
@@ -1,18 +1,5 @@
 $( document ).ready(function() {
-    //On load grab the window size
-    let windowSize = $(window).width();
-    // Check window size against typical mobile size and toggle sidebar
-    // if(windowSize <= 768){
-    //     $('#wrapper').toggleClass("toggled");
-    //     $('.sidebar-heading').toggleClass("d-none");
-    // }
-    //When the window resizes , re-check responsiveness
-    // $(window).on('resize', function(){
-    //   let win = $(this); 
-    //   if (win.width() <= 768) {
-    //   $('#wrapper').toggleClass("toggled");
-    //   $('.sidebar-heading').toggleClass("d-none");}
-    // });
+    
     //Toggle menu large or small
     $('#menu-toggle').click(function(e) {
         e.preventDefault();
